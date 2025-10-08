db55293595a785f5e7a67a7087fdfe8d9e8176b3
Azekowka <gabjtov@gmail.com>
2025-10-08 23:26:23 +0500

    feat(landing): FeaturesSlide

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,12 +5,14 @@ import Testimonials from "@/components/home/testimonials";
 import Pricing from "@/components/home/pricing";
 import FAQ from "@/components/home/faq";
 import Footer from "@/components/home/footer";
+import FeaturesSlide from "@/components/home/features-slide";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<Testimonials />
+			<FeaturesSlide />
 			<Pricing />
 			<FAQ />
 			<Footer />
